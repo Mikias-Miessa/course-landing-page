daba5d0be93dc68d991efa997b99e9cf6e756633
Mikias Miessa <mikiasmiessa@gmail.com>
2023-12-12 12:53:55 +0300

    adding overview page

---
+++ src/app/curriculum.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Curriculum = () => {
+  return (
+    <div className='h-screen bg-white'>curriculum</div>
+  )
+}
+
+export default Curriculum
--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import hero from '../../public/hero.jpg'
 import Link from 'next/link';
 import Hero from './hero';
 import OverView from './overview';
+import Curriculum from './curriculum';
 
 export default function Home() {
 
   return (
     <div className='bg-white'>
       <Hero />
-      <OverView/>
+      <OverView />
+      <Curriculum/>
     </div>
     
    
