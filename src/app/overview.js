fbe52983a0a1ac5c3c0e673dde4de2c760066de7
Mikias Miessa <mikiasmiessa@gmail.com>
2023-12-14 20:37:49 +0300

    trying to make the overlay page

--- src/app/overview.js
+++ src/app/overview.js
@@ -1,9 +1,10 @@
 import React from 'react';
+// Import your CSS file
 
 const OverView = () => {
   return (
-    <div className='h-screen bg-white text-black grid lg:col-5 px-10 '>
-      <div className='hidden lg:block lg:col-span-1 bg-black '></div>
+    <div className='relative h-screen bg-white text-black grid lg:grid-cols-5 px-10 '>
+      <div className='hidden lg:block lg:col-span-1 sticky-top'></div>
       <div className='lg:col-span-4'></div>
     </div>
   );
