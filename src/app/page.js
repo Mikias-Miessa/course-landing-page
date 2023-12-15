d55398a3c664f55e6e2469d51f5cb502d4698ee7
hanna kebede <se.hanna.kebede@gmail.com>
2023-12-15 08:53:33 +0300

    curriculum

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import hero from '../../public/hero.jpg'
 import Link from 'next/link';
 import Hero from './hero';
+import Curriculum from './curriculum';
 
 export default function Home() {
 
   return (
     <div>
       <Hero/>
+      {/* <Curriculum/> */}
     </div>
     
    
